df4259bff926648596a5a9ef6311514de7f54677
Pedro Henrique Barbosa <42444813+PdroHenrique@users.noreply.github.com>
2018-09-10 22:06:32 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,51 @@
+//Pedro Henrique de Paula Barbosa
+//11811EAU012
+
+#include <stdio.h>
+
+int main()
+{
+	int estado = 0, i;
+	char bits[256];
+	printf("Insira a sequencia de bits: ");
+	scanf("%s", bits);
+	for(i=0;bits[i] !=  '\0'; i++)
+	{
+		if(estado==0&&bits[i]=='0')
+		{
+			estado = 0;
+		}
+		else if(estado==0&&bits[i]=='1')
+		{
+			estado = 1;
+		}
+		else if(estado==1&&bits[i]=='0')
+		{
+			estado = 2;
+		}
+		else if(estado==1&&bits[i]=='1')
+		{
+			estado = 0;
+		}
+		else if(estado==2&&bits[i]=='1')
+		{
+			estado = 2;
+		}
+		else if(estado==2&&bits[i]=='0')
+		{
+			estado = 1;
+		}
+	}
+	printf("O valor da variavel e: %d", estado);
+	printf("\nA sequecia original e: %s", bits);
+	if(estado==0)
+	{
+		printf("\nE multiplo de 3");
+	}
+	else
+	{
+		printf("\nNao e");
+	}
+	getch();
+	return 0;
+}
